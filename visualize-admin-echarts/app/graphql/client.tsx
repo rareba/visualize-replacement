9083c6f463eecae68f8fb75330e26f33abac2ca9
Giulio V <vannini.gv@gmail.com>
2026-01-26 16:41:24 +0100

    feat: add GraphQL query batching to reduce HTTP requests
    - Add custom batchExchange that batches queries within 10ms window
    - Multiple GraphQL queries now sent in single HTTP request
    - Apollo Server handles batched requests natively
    - Can disable with ?flag_disable_batch=true for debugging

--- visualize-admin-echarts/app/graphql/client.tsx
+++ visualize-admin-echarts/app/graphql/client.tsx
@@ -1,13 +1,24 @@
-import { createClient, defaultExchanges } from "urql";
+import { createClient, cacheExchange, fetchExchange } from "urql";
 
 import { GRAPHQL_ENDPOINT } from "@/domain/env";
 import { flag } from "@/flags/flag";
 // @ts-ignore - dynamic package import based on NODE_ENV
 import { devtoolsExchanges } from "@/graphql/devtools";
+import { batchExchange } from "@/graphql/batch-exchange";
+
+// Batching is enabled by default - batches queries within 10ms window
+// Disable with ?flag_disable_batch=true in URL for debugging
+const useBatching = !flag("disable_batch");
 
 export const client = createClient({
   url: GRAPHQL_ENDPOINT,
-  exchanges: [...devtoolsExchanges, ...defaultExchanges],
+  exchanges: [
+    ...devtoolsExchanges,
+    cacheExchange,
+    // Batch exchange collects queries and sends them in a single HTTP request
+    ...(useBatching ? [batchExchange({ maxDelay: 10, maxSize: 20 })] : []),
+    fetchExchange,
+  ],
   fetchOptions: {
     headers: getHeaders(),
   },
